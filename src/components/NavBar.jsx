cd050ee5db9987630f8007df1e8bf2e36c34aac4
PopovicJovan <jovan6.p@gmail.com>
2025-03-03 08:52:57 +0100

    make navbar available for small and medium size  devices

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,14 @@ const NavBar = () => {
 
 
     return (
-        <nav className={styles['nav'] + " px-5 flex justify-center items-center"}>
-            <span className={"block md:hidden absolute z-50 top-2 start-2 text-4xl lg:text-5xl font-serif font-bold text-white uppercase p-0 m-0 tracking-tighter "}>
-                    JoRest
-            </span>
-            <ul className={"flex gap-x-12 " + styles['nav-list'] }>
+        <nav className={styles['nav'] + " absolute px-2 md:px-5 flex justify-center items-center top-16 md:top-0 w-full border-1 border-t-gray-600 border-b-gray-600 md:border-0  "}>
+            <ul className={"w-full flex justify-center gap-x-2 md:gap-x-12 " + styles['nav-list'] }>
                 <li className={"flex justify-center items-center"}>
                     <Link to={"/"} >HOME</Link>
                 </li>
                 <li className={"flex justify-center items-center "} >MENU</li>
                 <li className={"flex justify-center items-center "}>
-                    <span className={"text-4xl lg:text-5xl font-serif font-bold uppercase p-0 m-0 tracking-tighter "}>
+                    <span className={"hidden md:block  text-4xl lg:text-5xl font-serif font-bold uppercase p-0 m-0 tracking-tighter "}>
                     JoRest
                     </span>
                 </li>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -12,6 +12,9 @@ const MainPage = () => {
                 <div className={"fixed top-0 right-0 me-2 mt-2 md:mt-16 z-50"}>
                     <MakeReservationButton text={"reserve table"}/>
                 </div>
+                <span className={"block md:hidden absolute z-50 top-2 start-2 text-4xl lg:text-5xl font-serif font-bold text-white uppercase p-0 m-0 tracking-tighter "}>
+                    JoRest
+                </span>
                 <div className={styles['mainpage-overlay']}></div>
                 <NavBar />
                 <div className={"p-0 m-0 relative top-1/4 text-center "}>
